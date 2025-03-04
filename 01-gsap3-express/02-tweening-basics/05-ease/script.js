f2cc536e05a6996a16be7655379e29d4f8358908
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-04 10:46:35 -0300

    March 4

--- 01-gsap3-express/02-tweening-basics/05-ease/script.js
+++ 01-gsap3-express/02-tweening-basics/05-ease/script.js
@@ -1,6 +1,6 @@
 
 
-gsap.to('.green', {duration:2, x:400, repeat:-1, yoyo:true})
+gsap.to('.green', {duration:2, x:400, ease:'step'})
 
-gsap.to('.pink', {duration:2, x:400, delay:0.5})
+gsap.to('.pink', {duration:2, x:400, ease:'linear'})
 
