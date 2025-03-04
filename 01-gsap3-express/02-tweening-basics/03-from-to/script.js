b11fe1536419f19f653dbfa6a643b18206fa0a66
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-03 21:38:32 -0300

    March 3

--- 01-gsap3-express/02-tweening-basics/03-from-to/script.js
+++ 01-gsap3-express/02-tweening-basics/03-from-to/script.js
@@ -1,2 +1,4 @@
-gsap.from('.fred', {x:400, y:200, scale:3, duration:3})
+// gsap.from('.fred', {x:400, y:200, scale:3, duration:3})
+
+gsap.fromTo('.fred', {x:700, y:400, opacity:0}, {x:400, y:200, scale:3, opacity:1, duration:3})
 
