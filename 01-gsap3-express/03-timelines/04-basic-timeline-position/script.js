b78139bfdacb7bf98f66cc226a946f8c5838c3c1
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-05 14:22:47 -0300

    March 5

--- 01-gsap3-express/03-timelines/04-basic-timeline-position/script.js
+++ 01-gsap3-express/03-timelines/04-basic-timeline-position/script.js
@@ -4,8 +4,13 @@ let animation = gsap.timeline()
     .from('#demo', {duration:1, opacity:0})
     .from('#title', {duration:1, opacity:0, scale:0, ease:'back'})
     .from('#freds img', {y:160, stagger:0.1, duration:0.8, ease:'back' }, '+=2')
+    .add('test',)
     .from('#time', {xPercent:100, duration:0.8}, '<+1')
+
+
 
 document.getElementById('play').onclick = ()=> animation.play();
 document.getElementById('pause').onclick = ()=> animation.pause();
-documeq.getElementById('reverse').onclick = ()=> animation.restart();
+document.getElementById('reverse').onclick = ()=> animation.reverse();
+document.getElementById('restart').onclick = ()=> animation.restart();
+document.getElementById('test').onclikc = ()=> animation.test();
