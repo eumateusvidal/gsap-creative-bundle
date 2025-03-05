15fb2f3580cea3d87010c9798b249e7fd0e08ee0
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-05 16:39:49 -0300

    March 5

--- 01-gsap3-express/04-animated-buttons/03-constant-hover/script.js
+++ 01-gsap3-express/04-animated-buttons/03-constant-hover/script.js
@@ -2,12 +2,12 @@ const cta = document.querySelector('.cta')
 
 // create a repeating scale tween
 
-const scaleTween = gsap.to(cta, {scale:1, paused:true});
+const scaleTween = gsap.to(cta, {scale:1, repeat:15, yoyo:true, paused:true});
 
 cta.addEventListener('mouseenter', function() {
     scaleTween.restart()
 })
 
 cta.addEventListener('mouseleave', function() {
-    scaleTween.reverse()
+    gsap.to(cta, {scale:0.8})
 })
