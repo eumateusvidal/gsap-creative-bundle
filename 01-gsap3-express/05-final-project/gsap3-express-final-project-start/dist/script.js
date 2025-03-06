2f59d989065ea9b3e12df26f8ea8022ad85fb733
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-06 15:22:54 -0300

    March 6

--- 01-gsap3-express/05-final-project/gsap3-express-final-project-start/dist/script.js
+++ 01-gsap3-express/05-final-project/gsap3-express-final-project-start/dist/script.js
@@ -1,1 +1,7 @@
-//https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com
+let tl = gsap.timeline({defaults:{opacity:0, ease:"back"}});
+  tl.from('#demo', {ease:'linear'})
+    .from('h1', {x:80})
+    .from('h2', {x:-80})
+    .from('p', {y:30})
+    .from('button', {y:30})
+    .from("#items > g", {scale:0, transformOrigin:'50% 50%', stagger:0.1})
