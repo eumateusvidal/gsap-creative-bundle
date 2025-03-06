9169b3451b49a5924e62f5f3cbf5bd7cdb254818
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-06 16:18:18 -0300

    March 6

--- 01-gsap3-express/05-final-project/gsap3-express-final-project-start/dist/script.js
+++ 01-gsap3-express/05-final-project/gsap3-express-final-project-start/dist/script.js
@@ -1,7 +1,17 @@
-let tl = gsap.timeline({defaults:{opacity:0, ease:"back"}});
-  tl.from('#demo', {ease:'linear'})
-    .from('h1', {x:80})
-    .from('h2', {x:-80})
-    .from('p', {y:30})
-    .from('button', {y:30})
-    .from("#items > g", {scale:0, transformOrigin:'50% 50%', stagger:0.1})
+var tl = gsap.timeline({ repeat:4, yoyo:true, defaults:{opacity:0, ease:"back"}});
+gsap.set("#demo", {rotationY:15})
+function init() {
+    tl.from("#demo", {ease:"linear", autoAlpha:0})
+    .from("h1", {x:80, duration:1})
+    .from("h2", {x:-80, duration:1}, "<")
+    .from("p", {y:30}, "-=0.2")
+    .from("button", {y:50}, "-=0.4") 
+    .from("#items > g", {scale:0, transformOrigin:"50% 50%", stagger:0.1}, "-=0.5")
+}
+
+window.addEventListener("load", function(event) { 
+  init(); //do stuff
+  GSDevTools.create({animation:tl})
+});
+
+//https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com
