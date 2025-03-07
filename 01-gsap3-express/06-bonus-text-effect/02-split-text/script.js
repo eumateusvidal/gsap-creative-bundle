7d13b590d2c36de1dafc8be44b2549ddc5b7a860
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-07 10:38:49 -0300

    March 7

---
+++ 01-gsap3-express/06-bonus-text-effect/02-split-text/script.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(GSDevTools, SplitText)
+  let split
+  let animation = gsap.timeline({})
+
+  function init() {
+    gsap.set('wrapper', {autoAlpha:1})
+    split = new SplitText('h1', {tipe:'chars'})
+    GSDevTools.create({animation:animation})
+  }
+
+window.addEventListener('load', init)
