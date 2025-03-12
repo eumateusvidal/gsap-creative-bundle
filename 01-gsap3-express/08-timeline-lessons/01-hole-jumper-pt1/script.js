1c27d9b5a67c5d428b49b1173a0dd0dde8e71e42
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-11 22:24:29 -0300

    March 11

--- 01-gsap3-express/08-timeline-lessons/01-hole-jumper-pt1/script.js
+++ 01-gsap3-express/08-timeline-lessons/01-hole-jumper-pt1/script.js
@@ -1,9 +1,16 @@
-const hole = document.querySelector('.hole')
-const herman = document.querySelector('.herman')
+const hole = document.querySelector(".hole")
+const herman = document.querySelector(".herman")
+const shadow = document.querySelector(".shadow") 
+const tl = gsap.timeline({repeat:1, yoyo:true, repeatDelay:0.5})
 
-const tl = gsap.timeline()
-.from(hole, {scale:0, repeat:1, yoyo:true})
-.fromTo(herman, {y:110}, {y:-175}, 0.2)
-.to(herman, {y:0, ease:'power1.in'}, '>')
+tl.from(hole, {scale:0, repeat:1, yoyo:true})
+  .fromTo(herman, {y:160, scaleY:2}, {y:-175, scaleY:1}, 0.2)
+  .to(herman, {y:-4, ease:"power1.in"}, ">")
+  .to(herman, {scaleY:0.8, scaleX:1.3, duration:0.2, transformOrigin:"50% 100%", repeat:1, yoyo:true})
+  .to(shadow, {opacity:1, duration:0.2}, 0.7)
+  .to(shadow, {scaleX:0.7, ease:"power1.in"}, ">")
+  
 
-GSDevTools.create({animation:tl});
+
+
+GSDevTools.create({})
